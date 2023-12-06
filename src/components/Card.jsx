0c60575a16ebe6f65b6d0f566f98a4c7676fbb07
mfbadr <mfbadr@gmail.com>
2023-12-06 14:26:25 -0500

    "style"

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,9 @@
 export default function Card({card}){
-    //>
-    //types
-    ////audo -> has audio url, title, label
-    ////video -> has videoid. title, text
-    //text -> has text & label
-    //opt_text -> has title and text
-    //
-
-    // green #54AE7E
-    // blue #C8E1FC
-    // yello #EDC745
-
     function getContent(){
       switch(card.type){
         case('audio'): 
           return (
-            <audio controls>
+            <audio  controls>
               <source src={card.audio_url} type="audio/mpeg" />
             </audio>
           )
@@ -48,18 +36,12 @@ export default function Card({card}){
 
 
     return (
-        <div className={`${getBGColor()}  card border-2 border-gray-300  p-6 mb-4 w-full  sm:rounded-xl flex flex-col justify-center`}>
+        <div className={`${getBGColor()}  card border-2 border-gray-300  p-6 mb-4 w-full  rounded-xl flex flex-col justify-center`}>
             <h2 className="mb-4 font-semibold text-xl ">{card.title || card.label}</h2>
             <div className="flex-grow flex items-center justify-center flex-col">
               {card.text && <p className="mb-4">{card.text}</p>}
-              <div className="">
-                {getContent()}
-
-              </div>
-              
-
+              {getContent()}
             </div>
-
         </div>
     )
 }
